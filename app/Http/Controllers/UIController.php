5628b57341865dbacae5b49e8eba3c700971e4f1
moizatharsoftcube <moizatharecoders12@gmail.com>
2022-07-01 18:59:49 +0500

    Reviews Added on Share your ultimate experience

--- app/Http/Controllers/UIController.php
+++ app/Http/Controllers/UIController.php
@@ -39,11 +39,87 @@ class UIController extends EmailController
     {
         return view('about-us');
     }
-    public function reviews()
+//    public function reviews()
+//    {
+//        $reviews = ReviewModel::where('status', 1)->with('getReviewUser')->get();
+//        $review_star = ReviewModel::where('status', 1)->pluck('star')->avg();
+//        $review_avg = number_format((float)$review_star, 1, '.', '');
+//        $excellent = ReviewModel::where('star', 5)->where('status', 1)->pluck('star')->count();
+//        $good = ReviewModel::where('star', 4)->where('status', 1)->pluck('star')->count();
+//        $average = ReviewModel::where('star', 3)->where('status', 1)->pluck('star')->count();
+//        $poor = ReviewModel::where('star', 2)->where('status', 1)->pluck('star')->count();
+//        $terrible = ReviewModel::where('star', 1)->where('status', 1)->pluck('star')->count();
+//
+//        //Excellent
+//        if($excellent  <= 0){$excellent_bar = 'bar-1';}
+//        elseif ($excellent < 10  && $excellent > 0){$excellent_bar = 'bar-2';}
+//        elseif ($excellent < 30  && $excellent > 10){$excellent_bar = 'bar-3';}
+//        elseif ($excellent < 60  && $excellent > 30){$excellent_bar = 'bar-4';}
+//        elseif ($excellent <= 100  && $excellent > 60){$excellent_bar = 'bar-5';}
+//        elseif ($excellent > 100){$excellent_bar = 'bar-6';}
+//
+//        //Good
+//        if($good  <= 0){$good_bar = 'bar-1';}
+//        elseif ($good < 10  && $good > 0){$good_bar = 'bar-2';}
+//        elseif ($good < 30  && $good > 10){$good_bar = 'bar-3';}
+//        elseif ($good < 60  && $good > 30){$good_bar = 'bar-4';}
+//        elseif ($good <= 100  && $good > 60){$good_bar = 'bar-5';}
+//        elseif ($good > 100){$good_bar = 'bar-6';}
+//
+//        //Average
+//        if($average  <= 0){$average_bar = 'bar-1';}
+//        elseif ($average < 10  && $average > 0){$average_bar = 'bar-2';}
+//        elseif ($average < 30  && $average > 10){$average_bar = 'bar-3';}
+//        elseif ($average < 60  && $average > 30){$average_bar = 'bar-4';}
+//        elseif ($average <= 100  && $average > 60){$average_bar = 'bar-5';}
+//        elseif ($average > 100){$average_bar = 'bar-6';}
+//
+//        //Poor
+//        if($poor  <= 0){$poor_bar = 'bar-1';}
+//        elseif ($poor < 10  && $poor > 0){$poor_bar = 'bar-2';}
+//        elseif ($poor < 30  && $poor > 10){$poor_bar = 'bar-3';}
+//        elseif ($poor < 60  && $poor > 30){$poor_bar = 'bar-4';}
+//        elseif ($poor <= 100  && $poor > 60){$poor_bar = 'bar-5';}
+//        elseif ($poor > 100){$poor_bar = 'bar-6';}
+//
+//        //Terrible
+//        if($terrible  <= 0){$terrible_bar = 'bar-1';}
+//        elseif ($terrible < 10  && $terrible > 0){$terrible_bar = 'bar-2';}
+//        elseif ($terrible < 30  && $terrible > 10){$terrible_bar = 'bar-3';}
+//        elseif ($terrible < 60  && $terrible > 30){$terrible_bar = 'bar-4';}
+//        elseif ($terrible <= 100  && $terrible > 60){$terrible_bar = 'bar-5';}
+//        elseif ($terrible > 100){$terrible_bar = 'bar-6';}
+//
+//        return view('reviews', compact('reviews', 'review_avg','excellent','good','average','poor','terrible','excellent_bar','good_bar','average_bar','poor_bar','terrible_bar'));
+//    }
+
+    public function submit_review(Request $request)
+    {
+        $request->validate([
+            'message' => 'required|max:1000',
+            'rating' => 'required|numeric|min:1|max:5'
+        ]);
+        if (Auth::check() && Auth::user()->id) {
+            $review_model = new ReviewModel();
+            $review_model->user_id = Auth::user()->id;
+            $review_model->message = $request->message;
+            $review_model->star = $request->rating;
+            $review_model->status = 0;
+            $review_model->save();
+            if ($review_model->save()) {
+                return redirect()->route('UI_reviews')->with('success', 'submitted');
+            }
+        } else {
+            return redirect()->route('UI_reviews')->with('error', 'Login Requried');
+        }
+        return redirect()->route('UI_index');
+    }
+    public function share_experience()
     {
         $reviews = ReviewModel::where('status', 1)->with('getReviewUser')->get();
         $review_star = ReviewModel::where('status', 1)->pluck('star')->avg();
         $review_avg = number_format((float)$review_star, 1, '.', '');
+
         $excellent = ReviewModel::where('star', 5)->where('status', 1)->pluck('star')->count();
         $good = ReviewModel::where('star', 4)->where('status', 1)->pluck('star')->count();
         $average = ReviewModel::where('star', 3)->where('status', 1)->pluck('star')->count();
@@ -90,35 +166,8 @@ class UIController extends EmailController
         elseif ($terrible <= 100  && $terrible > 60){$terrible_bar = 'bar-5';}
         elseif ($terrible > 100){$terrible_bar = 'bar-6';}
 
-        return view('reviews', compact('reviews', 'review_avg','excellent','good','average','poor','terrible','excellent_bar','good_bar','average_bar','poor_bar','terrible_bar'));
+        return view('share-experience', compact('reviews', 'review_avg','excellent','good','average','poor','terrible','excellent_bar','good_bar','average_bar','poor_bar','terrible_bar'));
     }
-
-    public function submit_review(Request $request)
-    {
-        $request->validate([
-            'message' => 'required|max:1000',
-            'rating' => 'required|numeric|min:1|max:5'
-        ]);
-        if (Auth::check() && Auth::user()->id) {
-            $review_model = new ReviewModel();
-            $review_model->user_id = Auth::user()->id;
-            $review_model->message = $request->message;
-            $review_model->star = $request->rating;
-            $review_model->status = 0;
-            $review_model->save();
-            if ($review_model->save()) {
-                return redirect()->route('UI_reviews')->with('success', 'submitted');
-            }
-        } else {
-            return redirect()->route('UI_reviews')->with('error', 'Login Requried');
-        }
-        return redirect()->route('UI_index');
-    }
-    public function share_experience()
-    {
-        return view('share-experience');
-    }
-
 
     public function service_provider()
     {
